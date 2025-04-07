bd4ac95dd532194d4c35d75903ad9928edcf3e74
andreluke <andre.l.sales2706@gmail.com>
2025-04-06 21:14:20 -0300

    :wrench: chore: Exemplo de rota para se utilizar

---
+++ src/routes/example/hello-world.ts
@@ -0,0 +1,46 @@
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import z from 'zod'
+import { ExampleController } from '#/controllers/ExampleController'
+import { StatusCodes } from '#/enums/status-code'
+import { catchError } from '#/utils/catchError'
+
+export const helloWorldRoute: FastifyPluginAsyncZod = async app => {
+  app.get(
+    '/hello-world',
+    {
+      schema: {
+        summary: 'Example route',
+        tags: ['Example'],
+        operationId: 'helloWorld',
+        // body: z.object({}),
+        // params: z.object({}),
+        // query: z.object({}),
+
+        response: {
+          [StatusCodes.OK]: z.object({
+            message: z.string(),
+          }),
+          [StatusCodes.INTERNAL_SERVER_ERROR]: z.object({
+            message: z.string(),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      // Testem essa função de diferentes formas mudando de false para true
+      const exampleController = new ExampleController(false)
+
+      const [error, data] = await catchError(exampleController.getHelloWorld())
+
+      if (error) {
+        return reply.status(error.statusCode).send({
+          message: error.message,
+        })
+      }
+
+      return reply.status(StatusCodes.OK).send({
+        message: data,
+      })
+    }
+  )
+}
